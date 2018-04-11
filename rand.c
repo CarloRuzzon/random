46373c96b1a1016dc3f9ed2fae8182e9bca00c59
carloruzzon <carloruzzon@gmail.com>
2018-04-11 23:12:12 +0200

    fatto

---
+++ rand.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int num, i;
+  srand ( time(NULL) );
+
+  for(i = 0; i < 6; i++){
+    num = 50 + rand() % 6;
+    printf("%d\t", num);
+  }
+printf("\n");
+  for(i = 0; i < 5; i++){
+    num = (1 + rand() % 5) * 2;
+    printf("%d\t", num);
+  }
+printf("\n");
+  for(i = 0; i < 5; i++){
+    num = (1 + (1 + rand() % 5) * 2);
+    printf("%d\t", num);
+  }
+  printf("\n");
+  for(i = 0; i < 5; i++){
+    num = (2 * (1 + (1 + rand() % 5) * 2));
+    printf("%d\t", num);
+  }
+  return 0;
+}
